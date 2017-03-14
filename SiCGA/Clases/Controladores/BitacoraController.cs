cccf1827b3aae4195238a51f80c5748f541f2d81
Alan Adalberto Ortiz <ortizalan@gmail.com>
2017-03-13 23:55:11 -0700

    initial commit

---
+++ SiCGA/Clases/Controladores/BitacoraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiCGA.Clases.Controladores
+{
+    class BitacoraController : Metodos
+    {
+		public BitacoraController()
+		{
+			
+		}
+
+        ~BitacoraController() {
+        }
+
+
+        public override bool ConsultarRegistro(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ActualizarRegistro(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IngresarRegisto(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ConsultarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SiCGA/Inicio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SiCGA.Clases.Controladores;
+using SiCGA.Clases.Modelos;
+
+namespace SiCGA
+{
+    public partial class Inicio : Form
+    {
+        UsuariosController cUser = new UsuariosController();
+        UsuariosModel mUser = new UsuariosModel();
+    
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            mUser.Id = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mUser.Id = 1;
+            Object u = mUser;
+
+            if (cUser.ConsultarRegistro(u))
+            {
+                MessageBox.Show("el usuario es :".ToUpper() + cUser.Tabla.Rows[0]["Nombre"].ToString());
+            }
+            else
+            {
+                MessageBox.Show("ocurrió el siguiente error :".ToUpper() + cUser.Error);
+            }
+           
+            
+        }
+    }
+}
