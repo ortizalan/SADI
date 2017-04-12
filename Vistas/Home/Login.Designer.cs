81b535ca205c695fb5f43b6fcac48df9b4bb03fe
Ing. Alan Ortiz <ortizalan@gmail.com>
2017-04-12 14:59:23 -0700

    login sistema

---
+++ Vistas/Home/Login.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SADI.Vistas.Home
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.cmdIN = new System.Windows.Forms.Button();
+            this.cmdOUT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USUARIO :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONTRASEÑA :";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(166, 82);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(148, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(166, 118);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(148, 20);
+            this.txtContraseña.TabIndex = 3;
+            // 
+            // cmdIN
+            // 
+            this.cmdIN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdIN.Location = new System.Drawing.Point(268, 189);
+            this.cmdIN.Name = "cmdIN";
+            this.cmdIN.Size = new System.Drawing.Size(120, 60);
+            this.cmdIN.TabIndex = 4;
+            this.cmdIN.Text = ":: INGRESAR ::";
+            this.cmdIN.UseVisualStyleBackColor = true;
+            // 
+            // cmdOUT
+            // 
+            this.cmdOUT.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdOUT.Location = new System.Drawing.Point(392, 189);
+            this.cmdOUT.Name = "cmdOUT";
+            this.cmdOUT.Size = new System.Drawing.Size(120, 60);
+            this.cmdOUT.TabIndex = 5;
+            this.cmdOUT.Text = ":: SALIR ::";
+            this.cmdOUT.UseVisualStyleBackColor = true;
+            this.cmdOUT.Click += new System.EventHandler(this.cmdOUT_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 261);
+            this.Controls.Add(this.cmdOUT);
+            this.Controls.Add(this.cmdIN);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.Text = ":: INGRESE AL SISTEMA ::";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button cmdIN;
+        private System.Windows.Forms.Button cmdOUT;
+    }
+}
