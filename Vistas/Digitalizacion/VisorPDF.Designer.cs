9dcb58374a66b656b030f36aa46da1eab7d72cee
Alan Adalberto Ortiz Pérez <ortizalan@hotmail.com>
2018-05-02 09:00:49 -0700

    Visor PDF "OK"

--- Vistas/Digitalizacion/VisorPDF.Designer.cs
+++ Vistas/Digitalizacion/VisorPDF.Designer.cs
@@ -28,41 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
-            this.pdfViewer1 = new PDFViewer.PdfViewer();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VisorPdf));
+            this.pdfControl1 = new ComponentPro.Pdf.PdfControl();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
             this.SuspendLayout();
             // 
-            // elementHost1
+            // axAcroPDF1
             // 
-            this.elementHost1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.axAcroPDF1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.elementHost1.Location = new System.Drawing.Point(1, 1);
-            this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(798, 446);
-            this.elementHost1.TabIndex = 0;
-            this.elementHost1.Text = "elementHost1";
-            this.elementHost1.Child = this.pdfViewer1;
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(150, 0);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(460, 454);
+            this.axAcroPDF1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // VisorPdf
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axAcroPDF1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "VisorPdf";
             this.ShowIcon = false;
             this.Text = "VisorPdf";
             this.Load += new System.EventHandler(this.VisorPdf_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
-        #endregion
+        private ComponentPro.Pdf.PdfControl pdfControl1;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.Button button1;
 
-        private System.Windows.Forms.Integration.ElementHost elementHost1;
-        private PDFViewer.PdfViewer pdfViewer1;
+        #endregion
+        //private PDFViewer.PdfViewer pdfViewer1;
     }
 }
--- Vistas/Digitalizacion/VisorPDF.cs
+++ Vistas/Digitalizacion/VisorPDF.cs
@@ -13,7 +13,18 @@ namespace SADI.Vistas
 
         private void VisorPdf_Load(object sender, EventArgs e)
         {
-            pdfViewer1.LoadFile(Utilerias.Path);
+            //axAcroPDF1.LoadFile(Utilerias.Path);
+            axAcroPDF1.src = Utilerias.Path;
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = "Archivos PDF(*.pdf)|*.pdf";
+            //DialogResult r = ofd.ShowDialog();
+            //if(r == DialogResult.OK)
+            //{
+            //    axAcroPDF1.LoadFile(ofd.FileName);
+            //}
+            axAcroPDF1.setShowToolbar(true);
         }
+
+       
     }
 }
